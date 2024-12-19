9a7d9ee583976f79b3a757fee4930a8519a7a05b
ahe <22-it1.heinrich@bbk.schule>
2024-12-19 12:16:05 +0100

    Test fuer Aenderung am Caching2

--- backend/api/views.py
+++ backend/api/views.py
@@ -73,7 +73,7 @@ class CustomerView(APIView):
         customers = Customer.objects.all()
         serializer = CustomerSerializer(customers, many=True)
         response = Response(serializer.data)
-        response["Cache-Control"] = "no-cache"
+        response["Cache-Control"] = "no-cache, no-store, must-revalidate"
         #response["Pragma"] = "no-cache"
         #response["Expires"] = "0"
         return response
